d910db2d7b2891930efb99cb454f785a5a46d7a9
jbcotard <jb.cotard@laposte.net>
2015-01-17 15:58:51 +0100

    ajout interface mapbuilder

--- urbanflow/src/main/java/fr/sesamvitale/l24hc2015/urbanflow/Main.java
+++ urbanflow/src/main/java/fr/sesamvitale/l24hc2015/urbanflow/Main.java
@@ -1,9 +1,21 @@
 package fr.sesamvitale.l24hc2015.urbanflow;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
-import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+import fr.sesamvitale.l24hc2015.urbanflow.map.MapBuilder;
 
 public class Main {
 
@@ -11,19 +23,80 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("hello world ! bozo");
+		System.out.println("Urban Flow");
+		
+		
+		// 0 - Genere la MAP
+		MapBuilder mapBuilder = null;
+		
+		// 2 - demande de jeu (connect )
+		
+		// 3 - Attente debut du jeu (verify)
+		//loop attente
+		// -> parse verify : statut partie + date + position init + position cible  + url
+		
+		
+		// 4 - jeu (move)
+		
+		// boucle sur des mouvements
+		// verify ? pour tester le timeout de la partie ??
+		// 4-1 - recuperation incidents
+		// 4-2 - regeneration map / recalcul itineraire
+		// 4-3 - move
+		// -> parse resultat move : url 
+		// 4-4 - affichage
+		
+		
+		
+		
 
+		String botvitale2_SECRET = "361581db8f84766d2912241aa17778fce64b91e05db18616877b2d44af3099aa";
+		String botvitale2_TOKEN = "ea3b31b7956ba470a0545b79e4069d71";
+
+		String responseBody = connectBOT(botvitale2_SECRET, botvitale2_TOKEN);
+		System.out.println(responseBody);
 		
 		
-		List<Object> providers = new ArrayList<Object>();
-	     providers.add( new JacksonJaxbJsonProvider() );
-	   
-//	     WebClient client = WebClient.create("http://localhost:8080/poc_restapi_cxf/api", providers);
-//	     client = client.accept("application/json").type("application/json").path("/order").query("id", "1");
-//	   
-//	     Order order = client.get(Order.class);
-//	     System.out.println("Order:" + order.getCustomerName());
-		
+
+	}
+
+	private static String connectBOT(String botvitale2_SECRET,
+			String botvitale2_TOKEN) {
+		String responseBody = null;
+		try {
+			URL url = new URL("http://24hc15.haum.org/api/connect/"
+					+ botvitale2_TOKEN);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setDoOutput(true);
+			conn.setRequestMethod("POST");
+			conn.setRequestProperty("Content-Type", "application/json");
+
+			String input = "{\"secret\":\"" + botvitale2_SECRET
+					+ "\",\"mode\":\"training\"}";
+
+			OutputStream os = conn.getOutputStream();
+			os.write(input.getBytes());
+			os.flush();
+
+			Scanner scanner;
+			String response;
+			if (conn.getResponseCode() != 200) {
+				scanner = new Scanner(conn.getErrorStream());
+				response = "Error From Server \n\n " + conn.getResponseCode();
+			} else {
+				scanner = new Scanner(conn.getInputStream());
+				response = "Response From Server \n\n";
+			}
+			scanner.useDelimiter("\\Z");
+			responseBody = response + scanner.next();
+			scanner.close();
+			conn.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return responseBody;
 	}
 
 }
---
+++ urbanflow/src/main/java/fr/sesamvitale/l24hc2015/urbanflow/map/MapBuilder.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package fr.sesamvitale.l24hc2015.urbanflow.map;
+
+/**
+ * @author jb
+ *
+ */
+public interface MapBuilder {
+
+}
