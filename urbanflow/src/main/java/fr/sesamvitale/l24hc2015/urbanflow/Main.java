533796b19074d000ff8e273e28e3b1f83fcb311e
jbcotard <jb.cotard@laposte.net>
2015-01-17 12:03:52 +0100

    init appli urbanflow

---
+++ urbanflow/src/main/java/fr/sesamvitale/l24hc2015/urbanflow/Main.java
@@ -0,0 +1,13 @@
+package fr.sesamvitale.l24hc2015.urbanflow;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello world ! bozo");
+
+	}
+
+}
