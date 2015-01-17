a799cec6aa8cf19db6cfa5cba9e1655d2e316018
jbcotard <jb.cotard@laposte.net>
2015-01-17 13:20:00 +0100

    ajout dependence cxf + jgraft

--- urbanflow/src/main/java/fr/sesamvitale/l24hc2015/urbanflow/Main.java
+++ urbanflow/src/main/java/fr/sesamvitale/l24hc2015/urbanflow/Main.java
@@ -1,5 +1,10 @@
 package fr.sesamvitale.l24hc2015.urbanflow;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+
 public class Main {
 
 	/**
@@ -8,6 +13,17 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello world ! bozo");
 
+		
+		
+		List<Object> providers = new ArrayList<Object>();
+	     providers.add( new JacksonJaxbJsonProvider() );
+	   
+//	     WebClient client = WebClient.create("http://localhost:8080/poc_restapi_cxf/api", providers);
+//	     client = client.accept("application/json").type("application/json").path("/order").query("id", "1");
+//	   
+//	     Order order = client.get(Order.class);
+//	     System.out.println("Order:" + order.getCustomerName());
+		
 	}
 
 }
