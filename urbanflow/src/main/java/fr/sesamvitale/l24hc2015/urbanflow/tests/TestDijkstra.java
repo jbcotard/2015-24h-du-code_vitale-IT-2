87e335e0d975bec0ca5ca2e1ed5cd25f4a6c76df
jbcotard <jb.cotard@laposte.net>
2015-01-24 11:00:41 +0100

    ajout test

---
+++ urbanflow/src/main/java/fr/sesamvitale/l24hc2015/urbanflow/tests/TestDijkstra.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package fr.sesamvitale.l24hc2015.urbanflow.tests;
+
+import org.junit.Test;
+
+import fr.sesamvitale.l24hc2015.urbanflow.data.Deplacement;
+import fr.sesamvitale.l24hc2015.urbanflow.data.Reseau;
+import fr.sesamvitale.l24hc2015.urbanflow.graphe.GrapheImpl;
+import fr.sesamvitale.l24hc2015.urbanflow.graphe.IGraphe;
+import fr.sesamvitale.l24hc2015.urbanflow.map.MapBuilder;
+import fr.sesamvitale.l24hc2015.urbanflow.map.MapBuilderImpl;
+
+/**
+ * @author user
+ *
+ */
+public class TestDijkstra {
+
+	@Test
+	public void testAlgorithme() {
+		MapBuilder mapBuilder = new MapBuilderImpl();
+		Reseau r = mapBuilder.buildReseau();
+		IGraphe g = GrapheImpl.getInstance();
+		g.creerGraphe(r);
+		Deplacement d = g.seDeplacer(1370, 1326, "07:46:00", "me");
+		
+		
+	}
+
+}
